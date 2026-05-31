1be49a770c7e2ac80d9e372c41e016d8274ee960
Sm64 Android <167822889+Christopher0765@users.noreply.github.com>
2026-05-31 18:58:29 -0400

    Add Spanish language tags to langdata

---
+++ lang-tags/tag-lang-es.lua
@@ -0,0 +1,23 @@
+if unsupported then return end
+
+langdata["es"] = {
+    tag_author = "[Autor de \\#ffff1e\\Coords\\#00ff00\\ + \\#00ffff\\DX\\#ff8800\\] ",
+    tag_lead_dev = "[Desarrollador Principal]",
+    tag_og_author = "[Autor del Flood Original, Testeador]",
+    tag_fe_author = "[Autor de FE, Desarrollador]",
+    tag_dev = "[Desarrollador]",
+    tag_dev_short = "[Dev]",
+    tag_flood_porter = "[Porter de Flood +]",
+    tag_flood_porter_contributor_tester = "[Porter de Flood +, Contribuidor, Testeador]",
+    tag_flood_porter_composer = "[Porter de Flood +, Compositor]",
+    tag_porter = "[Porter]",
+    tag_composer_tester = "[Compositor, Testeador]",
+    tag_tester = "[Testeador]",
+    tag_tester_special_thanks = "[Testeador, Gracias Especiales]",
+    tag_contributor = "[Contribuidor]",
+    tag_contributor_special_thanks = "[Contribuidor, Gracias Especiales]",
+    tag_special_thanks = "[Gracias Especiales]",
+    tag_normal = "[Normal]",
+    tag_translator = "[Traductor]",
+    
+}
