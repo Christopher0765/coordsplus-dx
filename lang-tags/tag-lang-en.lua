31e6f33302859875571dd959f4c6beb137cfe21c
Sm64 Android <167822889+Christopher0765@users.noreply.github.com>
2026-05-31 18:59:15 -0400

    Add files via upload

---
+++ lang-tags/tag-lang-en.lua
@@ -0,0 +1,23 @@
+if unsupported then return end
+
+langdata["en"] = {
+    tag_author = "[\\#ffff1e\\Coords\\#00ff00\\ + \\#00ffff\\DX\\#ff8800\\ Author] ",
+    tag_lead_dev = "[Lead Developer]",
+    tag_og_author = "[Original Flood Author, Tester]",
+    tag_fe_author = "[FE Author, Developer]",
+    tag_dev = "[Developer]",
+    tag_dev_short = "[Dev]",
+    tag_flood_porter = "[Flood Porter]",
+    tag_flood_porter_contributor_tester = "[Flood Porter, Contributor, Tester]",
+    tag_flood_porter_composer = "[Flood Porter, Composer]",
+    tag_porter = "[Porter]",
+    tag_composer_tester = "[Composer, Tester]",
+    tag_tester = "[Tester]",
+    tag_tester_special_thanks = "[Tester, Special Thanks]",
+    tag_contributor = "[Contributor]",
+    tag_contributor_special_thanks = "[Contributor, Special Thanks]",
+    tag_special_thanks = "[Special Thanks]",
+    tag_normal = "[Normal]",
+    tag_translator = "[Translator]",
+    
+}
---
+++ lang-tags/tag-lang-fr.lua
@@ -0,0 +1,23 @@
+if unsupported then return end
+
+langdata["fr"] = {
+    tag_author = "[Auteur de \\#ffff1e\\Coords\\#00ff00\\ + \\#00ffff\\DX\\#ff8800\\] ",
+    tag_lead_dev = "[Développeur Principal]",
+    tag_og_author = "[Auteur du Flood Original, Testeur]",
+    tag_fe_author = "[Auteur de Flood Expanded, Développeur]",
+    tag_dev = "[Développeur]",
+    tag_dev_short = "[Dev]",
+    tag_flood_porter = "[Porteur Flood +]",
+    tag_flood_porter_contributor_tester = "[Porteur Flood +, Contributeur, Testeur]",
+    tag_flood_porter_composer = "[Porteur Flood +, Composeur]",
+    tag_porter = "[Porteur]",
+    tag_composer_tester = "[Composeur, Testeur]",
+    tag_tester = "[Testeur]",
+    tag_tester_special_thanks = "[Testeur, Remerciments Spéciaux]",
+    tag_contributor = "[Contributeur]",
+    tag_contributor_special_thanks = "[Contributeurr, Remerciments Spéciaux]",
+    tag_special_thanks = "[Remerciments Spéciaux]",
+    tag_normal = "[Normal]",
+    tag_translator = "[Traducteur]",
+    
+}
